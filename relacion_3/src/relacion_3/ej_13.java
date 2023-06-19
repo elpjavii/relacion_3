16ccda18c0b0ac77eba16703941eb66bc90093c8
crossbow <crossbow@DESKTOP-MHIED8R>
2023-06-19 14:10:07 +0200

    .

--- relacion_3/src/relacion_3/ej_13.java
+++ relacion_3/src/relacion_3/ej_13.java
@@ -8,10 +8,11 @@ public class ej_13 {
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("pasa el numero:  ");
 		int num = teclado.nextInt();
-		
-		for(int i = 2; i <= Math.sqrt(num); i++) {
+		boolean primo=true;
+		for(int i = 2; i <= Math.sqrt(num) && primo; i++) {
 			if (num % i == 0) {
 				System.out.println("no es primo");
+				primo=false;
 				return;
 			}
 		}
